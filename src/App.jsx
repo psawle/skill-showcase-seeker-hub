09525d11b832a4074e1efbfc12b439695f7906dd
psawle <psawle4@gmail.com>
2025-07-10 12:50:51 +0530

    added 404.html

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={"production" ? "/skill-showcase-seeker-hub" : "/"}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
